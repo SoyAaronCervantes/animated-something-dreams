2f1156648dacdf9b75b75f0ed146244efa4fe468
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-03-05 17:25:01 -0600

    Init commit

---
+++ practica_tres/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  practica_tres
+//
+//  Created by Aarón Cervantes Álvarez on 05/03/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+
+}
+
---
+++ practica_tres/MyTabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyTabBarViewController.swift
+//  practica_tres
+//
+//  Created by Aarón Cervantes Álvarez on 05/03/21.
+//
+
+import UIKit
+
+class MyTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
